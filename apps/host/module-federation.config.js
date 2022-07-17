ffc30365ded18dd89a2de525a81ef41d4b3a9ceb
Bendy Zhang <zb@bndy.net>
2022-07-17 17:46:57 +0800

    feat(host): custom shared libs

--- apps/host/module-federation.config.js
+++ apps/host/module-federation.config.js
@@ -1,4 +1,21 @@
+const sharedLibraires = [
+  '@mfe/',  // Shared libraries, such as UiModule, please see /tsconfig.base.json paths node
+  '@angular/',
+  // Place the shared libraries here
+  // 'moment',
+];
+
 module.exports = {
   name: 'host',
   remotes: ['shop', 'cart', 'about'],
+  // By default, all packages in package.json will be as the shared,
+  // Here to override it.
+  shared: (libName, defaultConfig) => {
+    console.log(`'${libName}',`);
+    if (sharedLibraires.find(lib => libName.startsWith(lib))) {
+      return defaultConfig;
+    }
+    // Returning false means the library is not shared.
+    return false;
+  }
 };
